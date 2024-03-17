66b3e4428285b295cb195b4bb908705d58e00fc1
Per Fogelin <42115215+danribba@users.noreply.github.com>
2024-03-17 20:20:01 +0100

    Update name

--- app.py
+++ app.py
@@ -6,12 +6,12 @@ from links import *
   
 # Flask constructor takes the name of 
 # current module (__name__) as argument.
-my_app = Flask(__name__, template_folder='templates', static_folder='statics')
+app = Flask(__name__, template_folder='templates', static_folder='statics')
   
 # The route() function of the Flask class is a decorator, 
 # which tells the application which URL should call 
 # the associated function.
-@my_app.route('/')
+@app.route('/')
 # ‘/’ URL is bound with hello_world() function.
 
 
@@ -44,6 +44,6 @@ if __name__ == '__main__':
   
     # run() method of Flask class runs the application 
     # on the local development server.
-    my_app.run(debug="true")   
+    app.run(debug="true")   
 
 
